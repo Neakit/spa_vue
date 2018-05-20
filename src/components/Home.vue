62ab68abe0c3b436de210d9474e2a6228e45e4f7
Neakit <pavliukny@gmail.com>
2018-05-21 00:38:32 +0300

    init commit

---
+++ src/components/Home.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <v-container fluid>
+    <v-layout row>
+      <v-flex xs12>
+        <v-carousel>
+          <v-carousel-item 
+            v-for="ad in ads" 
+            :src="ad.imageSrc" 
+            :key="ad.id"
+          >
+            <div class="car-link">
+              <v-btn class="error" :to="'/ad/' + ad.id">{{ad.title}}</v-btn>
+            </div>
+          </v-carousel-item>
+        </v-carousel>
+      </v-flex>
+    </v-layout>
+    </v-container>
+    <v-container grid-list-lg>
+      <v-layout row wrap>
+        <v-flex 
+          v-for="ad in ads"
+          :key="ad.id"
+          sm6
+          md4
+          xs12
+        >
+          <v-card>
+            <v-card-media 
+              :src="ad.imageSrc" 
+              height="200px">
+            </v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">{{ad.title}}</h3>
+                <div>{{ad.description}}</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn flat :to="'/ad/' + ad.id">Open</v-btn>
+              <v-btn raised class="primary">Buy</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ads: [
+        {
+          title: 'First ad',
+          description: 'Hello am description',
+          promo: false,
+          imageSrc:
+            'https://vuetifyjs.com/static/doc-images/carousel/squirrel.jpg',
+          id: '1',
+        },
+        {
+          title: 'Second ad',
+          description: 'Hello am description',
+          promo: true,
+          imageSrc: 'https://vuetifyjs.com/static/doc-images/carousel/sky.jpg',
+          id: '2',
+        },
+        {
+          title: 'Third ad',
+          description: 'Hello am description',
+          promo: true,
+          imageSrc: 'https://vuetifyjs.com/static/doc-images/carousel/bird.jpg',
+          id: '3',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.car-link {
+  position: absolute;
+  bottom: 50px;
+  left: 50%;
+  background-color: rgba(0, 0, 0, 0.5);
+  transform: translate(-50%, 0);
+  padding: 5px 15px;
+  border-radius: 5px;
+}
+</style>
+
